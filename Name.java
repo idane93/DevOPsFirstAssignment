c362b52a2ca83fb86e12ca3eaf8d2d5e49467832
idane93 <idaneliyahu93@gmail.com>
2024-05-24 16:57:14 +0300

    update Name.java

--- Name.java
+++ Name.java
@@ -1,7 +1,7 @@
 public class Name{
 	public static void main(String[] args){
 		for (int i = 1; i <= 10; i++){
-			System.out.println("not bob");
+			System.out.println("still not bob");
 		}
 	}
 }
